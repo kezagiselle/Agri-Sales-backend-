01bbc8e7b3f484c658207f5df3f68738697d30b4
kezagiselle <kezagiselle776@gmail.com>
2024-05-14 12:08:50 +0200

    on Authentication

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import productRouter from './productRoutes.js';
 import userRouter from './user.js';
+import buyerRouter from './buyerSignUp.js';
+import farmerRouter from './FarmerSignUp.js';
+import tokenRouter from './userToken.js';
 
 const router = express.Router();
 
 router.use('/products',productRouter)
 router.use('/users',userRouter);
+router.use('/buyer',buyerRouter);
+router.use('/farmer',farmerRouter);
+router.use('/token',tokenRouter);
 
 export default router
--- utils/validation.js
+++ utils/validation.js
@@ -4,8 +4,43 @@ const addUserValidation = [
     body("firstName", "FirstName is required"),
     body("Address","Address is required")
 ];
+const signUpValidation = [
+    body("firstName", "First name is required").not().isEmpty(),
+    body("lastName", "Last name is required").not().isEmpty(),
+    body("email", "Email is required").not().isEmpty(),
+    body("email", "invalid email").isEmail(),
+    body("password", "password is required").not().isEmpty(),
+    body("password", "Password should contain atleast 8 characters, uppercase and lowercase letters").isStrongPassword()
+];
+const signInValidation = [
+    body("email", "Email is required").not().isEmpty(),
+    body("email", "invalid email").isEmail(),
+    body("password", "password is required").not().isEmpty(),
+    body("password", "invalid password").isStrongPassword()
+  ];
+  const otpValidation = [
+    body("otp","otp must be provided").not().isEmpty()
+];
+const forgotPasswordValidation = [
+    body("email", "Email must be provided").not().isEmpty(),
+];
+const resetPasswordValidation = [
+    body("password", "password must be provided").not().isEmpty(),
+    body("password", "Password should contain atleast 8 characters, uppercase and lower case letters, numbers, and symbols").isStrongPassword(),
+    // body("confirmPassword", "confirmPassword must be provided").not().isEmpty(),
+    // body("confirmPassword", "confirmPassword must be provided with atleast 8 characters").isStrongPassword(),
+    // body("confirmPassword", "Passwords do not match").custom((value, { req }) => value === req.body.password)   
+];
+
+
 
 const allValidation ={
-    addUserValidation
+    addUserValidation,
+    signUpValidation,
+    signInValidation,
+    otpValidation,
+    forgotPasswordValidation,
+    resetPasswordValidation
+
 };
 export default allValidation;
