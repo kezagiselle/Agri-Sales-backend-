e4237f9ac3a36c403ee8bb47d55d23bbd02ca9c0
kezagiselle <kezagiselle776@gmail.com>
2024-05-24 16:46:01 +0200

    debug

--- App.js
+++ App.js
@@ -7,14 +7,13 @@ import swaggerUI from "swagger-ui-express";
 import swaggerDocumentation from './docs/swagger.js';
 import ErrorHandler from './middleware/ErrorHandler.js';
 import cookieParser from 'cookie-parser';
-import cookieParser from 'cookie-parser';
 
 
 
+const app = express();
 
 app.use(express.json()); 
 // app.use(bodyParser)
-const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
@@ -26,11 +25,10 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocumentation));
 
 const start = async () => {
     try {
-     connectDB(); 
-        app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
+        connectDB()
+        app.listen(process.env.PORT, console.log(`server is listening on port ${process.env.PORT}`));
     } catch (error) {
-        console.error('Failed to start server:', error);
+       console.log(error)
     }
-};
-
+}
 start();
