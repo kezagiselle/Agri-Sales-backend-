60ba504e7213b4dea6141b0f3f7a2f8db4a622f9
sandrineuwumukiza <uwasandrine99@gmail.com>
2024-05-31 08:30:42 +0200

    checking multer function to upoad image

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,7 +6,7 @@ const productRouter = express.Router();
 
 
 
-productRouter.post('/addProduct', addProductValidator, upload.single('productImage'), AddProduct);
+productRouter.post('/addProduct', addProductValidator, upload.single('image'), AddProduct);
 productRouter.get('/productList', getProduct);
 productRouter.put('/updateProduct/:id',updateProductById);
 productRouter.get('/productById/:id', getProductById)
